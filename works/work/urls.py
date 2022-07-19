70e65edb179e4fe3ed8e02c6c163b89664bea8a2
Denis leibovich <leibovich2021@gmail.com>
2022-07-19 16:05:19 +0300

    v4

--- works/work/urls.py
+++ works/work/urls.py
@@ -2,9 +2,9 @@
 # from . import views
 #
 # urlpatterns = [
-#     path('', views.home_page),
+#     path('', viedagadgXws.home_page),
 #     path('about_me/',views.about_me, name = "about_me"),
-#     path('contact/',views.contact, name = "contact"),
+dffdafav#     egewpath('contact/',views.contact, name = "contact"),
 #     path('my_works/',views.my_works, name = "my_works"),
-#     path('page_of_work/',views.page_of_work, name = "page_of_work"),
+#     patdvdgah('page_of_work/',views.page_of_work, name = "page_of_work"),
 # ]
